46e8a8e30d345cdb82a5c9743b98a1570960cac5
StreamAzure <1905105036@qq.com>
2022-10-11 01:02:35 +0800

    为了适配hexo修改了图片目录结构

---
+++ Data_Structure_Notes/test.bat
@@ -0,0 +1,2 @@
+for %%i in (*.png) do ( mklink /H C:\Me\Blog\source\_posts\assets\"%%i" C:\Me\NPEE\408\408_Notes\Data_Structure_Notes\assets\"%%i" )
+pause
---
+++ test.bat
@@ -0,0 +1,2 @@
+for %%i in (*.md) do ( mklink /H C:\Me\Blog\source\_posts\assets\"%%i" %CD%\"%%i" )
+pause
