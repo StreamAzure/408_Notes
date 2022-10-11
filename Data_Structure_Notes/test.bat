57d0d07cd86e6338fdaf6f85c9a97cf8b7893b5f
StreamAzure <1905105036@qq.com>
2022-10-11 14:13:57 +0800

    update

--- Data_Structure_Notes/test.bat
+++
@@ -1,2 +0,0 @@
-for %%i in (*.png) do ( mklink /H C:\Me\Blog\source\_posts\assets\"%%i" C:\Me\NPEE\408\408_Notes\Data_Structure_Notes\assets\"%%i" )
-pause
--- test.bat
+++
@@ -1,2 +0,0 @@
-for %%i in (*.md) do ( mklink /H C:\Me\Blog\source\_posts\assets\"%%i" %CD%\"%%i" )
-pause
